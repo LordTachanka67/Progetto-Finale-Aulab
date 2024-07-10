06a7e7eb1026a8db0d737680783d9777aaba3be2
Debora <deboramasciave@gmail.com>
2024-07-10 18:03:28 +0200

    middleware, aggiunto un po' di stile. User Story 1 completata

--- presto_chiarolanza_masciave_mosca/resources/views/components/navbar.blade.php
+++ presto_chiarolanza_masciave_mosca/resources/views/components/navbar.blade.php
@@ -1,21 +1,22 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid nav-text">
+      <a class="navbar-brand" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box2-heart me-2 mb-2" viewBox="0 0 16 16">
+        <path d="M8 7.982C9.664 6.309 13.825 9.236 8 13 2.175 9.236 6.336 6.31 8 7.982"/>
+        <path d="M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4zm0 1H7.5v3h-6zM8.5 4V1h3.75l2.25 3zM15 5v10H1V5z"/> 
+      </svg>Presto.it</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
+            <a class="nav-link" aria-current="page" href="{{route('homepage')}}">Home</a>
           </li>
           @guest
           <li class="nav-item">
             <a class="nav-link" href="{{route('login')}}">Login</a>
           </li>              
           @endguest
-
-
           @auth  
           <form action="{{route('logout')}}" method="POST">
             @csrf <button class="nav-link">Logout</button>
@@ -44,7 +45,7 @@
         </ul>
         <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <button class="btn btn-quar" type="submit">Search</button>
         </form>
       </div>
     </div>
--- presto_chiarolanza_masciave_mosca/routes/web.php
+++ presto_chiarolanza_masciave_mosca/routes/web.php
@@ -9,7 +9,10 @@ Route::get('/', function () {
 
 /* ROTTE PER CATEGORIE */
 /* route::get('/categories/create',[ArticleController::class,'createCategory'])->name('categories.create'); */
-Route::controller(ArticleController::class)->prefix('articles')->group(function () {
-    Route::get('/create', 'create')->name('articles.create');
-    Route::post('/store', 'store')->name('articles.store');
+
+Route::middleware(['auth'])->group(function () {
+    Route::controller(ArticleController::class)->prefix('articles')->group(function () {
+        Route::get('/create', 'create')->name('articles.create');
+        Route::post('/store', 'store')->name('articles.store');
+    });   
 });
