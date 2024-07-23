d6cadf6955acaa9fee4d78d2feff45510dec7548
Debora <deboramasciave@gmail.com>
2024-07-23 19:48:20 +0200

    titoli team

--- presto_chiarolanza_masciave_mosca/resources/views/team.blade.php
+++ presto_chiarolanza_masciave_mosca/resources/views/team.blade.php
@@ -6,24 +6,24 @@
                 <h2 class="mb-3">DAG's dev</h2>
                 <h4 class="fst-italic fs-5">Tre sviluppatori, tre personalità uniche, un team imbattibile!</h4>
             </div>
-            <div class="col-4 text-center p-5">
-                <h3>Antonio Mosca</h3>
+            <div class="col-12 col-md-4 text-center p-5">
+                <h3 class="fs-3">Antonio Mosca</h3>
                 <h6 class="fst-italic">SIUM</h6>
                 <div><img class="teamPicture m-4 p-2 " src="/background/SIUM.jpeg" alt=""></div>
                 <p>Con la sua energia contagiosa, Antonio è sempre pronto a migliorare qualsiasi progetto urlando il suo
                     iconico "SIUM". Quando c'è bisogno di una spinta extra, lui è il vostro uomo!</p>
             </div>
 
-            <div class="col-4 text-center p-5">
-                <h3>Debora Masciavé</h3>
+            <div class="col-12 col-md-4 text-center p-5">
+                <h3 class="fs-4">Debora Masciavé</h3>
                 <h6 class="fst-italic">Facciamo pausa?</h6>
                 <div><img class="teamPicture m-4 p-2 " src="/background/Deb.jpg" alt=""></div>
                 <p>Debora è la regina delle pause strategiche, con il suo motto "facciamo una pausa?". Sa che un po' di
                     relax è essenziale per trovare le soluzioni migliori. Perfetta per bilanciare il lavoro e il
                     benessere!</p>
             </div>
-            <div class="col-4 text-center p-5">
-                <h3>Giovanni Chiarolanza</h3>
+            <div class="col-12 col-md-4 text-center p-5">
+                <h3 class="fs-3">Giovanni Chiarolanza</h3>
                 <h6 class="fst-italic">Metti margine 3</h6>
                 <div><img class="teamPicture m-4 p-2 " src="/background/giovanni.jpg" alt=""></div>
                 <p>Preciso e puntiglioso, Giovanni sa che il diavolo si nasconde nei dettagli. La sua frase "metti
